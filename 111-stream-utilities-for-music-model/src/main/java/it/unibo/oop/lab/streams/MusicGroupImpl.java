aba2c85bc310111d84b2e814e608895553c9393c
Cristian Qorri <cristian.qorri@studio.unibo.it>
2024-12-02 11:03:36 +0100

    implemented the method countSongs and countSongsInNoAlbum

--- 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -46,12 +46,15 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public int countSongs(final String albumName) {
-        return -1;
+        return (int) this.songs.stream()
+        .filter(song -> song.getAlbumName().isPresent())
+        .filter(song -> song.getAlbumName().get().equals(albumName)).count();
     }
 
     @Override
     public int countSongsInNoAlbum() {
-        return -1;
+        return (int) this.songs.stream()
+        .filter(song -> song.getAlbumName().isEmpty()).count();
     }
 
     @Override
