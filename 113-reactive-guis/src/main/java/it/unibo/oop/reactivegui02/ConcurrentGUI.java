6873119039a7664c0fdbf183fd6f999f5249a48e
Cristian Qorri <cristian.qorri@studio.unibo.it>
2024-12-04 21:45:49 +0100

    added up and down buttons, implemented up button, but missing to finish down button and use setEnabled

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,6 +2,7 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,4 +15,101 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class ConcurrentGUI extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+
+    /**
+     * Builds a new CGUI
+     */
+    public ConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        /*
+         * Create the counter agent and start it.
+         */
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+        /*
+         * Register a listener that stops it
+         */
+        stop.addActionListener(e -> agent.stopCounting());
+        /*
+         * Register a listener that up the incrementation
+         */
+        up.addActionListener(e -> agent.upCounting());
+        /*
+         * Register a listener that down the incrementation
+         */
+        down.addActionListener(e -> agent.downCounting());
+
+    }
+
+    /*
+     * The counter agent is implemented as a nested class.
+     */
+    private class Agent implements Runnable {
+        private volatile boolean stop;
+        private int counter;
+        private volatile boolean up = true;
+        
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    if (up) {
+                        counter++;
+                    } else {
+                        counter--;
+                    }
+                    // The EDT doesn't access `counter` anymore, it doesn't need to be volatile 
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                    this.counter++;
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    /*
+                     * This is just a stack trace print, in a real program there
+                     * should be some logging and decent error reporting
+                     */
+                    ex.printStackTrace();
+                }
+            }
+        }
+        
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        /**
+         * External command to up counting
+         */
+        public void upCounting() {
+            this.up = true;
+        }
+
+        /**
+         * External command to down counting
+         */
+        public void downCounting() {
+            this.up = false;
+        }
+    }
 }
